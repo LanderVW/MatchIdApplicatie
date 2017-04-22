de72ae695d244d241d74800da4ce267b9088672f
axel vulsteke <axel.vulsteke@student.kuleuven.be>
2017-04-22 14:09:27 +0200

    PictureViewFragment now we can see result in the app

--- app/src/main/java/com/matchid/matchidapplicatie/PictureViewFragment.java
+++ app/src/main/java/com/matchid/matchidapplicatie/PictureViewFragment.java
@@ -1,44 +1,29 @@
 package com.matchid.matchidapplicatie;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.util.Base64;
 
-import com.android.volley.toolbox.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.squareup.picasso.Picasso;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+import com.bumptech.glide.Glide;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 
 /**
  * Created by vulst on 18/04/2017.
@@ -55,20 +40,12 @@ public class PictureViewFragment extends Fragment {
     static final String KEY_NAME = "username";
     static final String KEY_COST = "cost";
     static final String KEY_DESC = "description";
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-    ArrayList<HashMap<String, String>> menuItems;
 
-    String url = "http://www.1080x1920wallpapers.com/1080x1920-backgrounds/1080x1920-wallpapers-2/1080x1920-HD-wallpapers-samsung-htc-android-smartphone-642srwg4-1080P.jpg";
-
+//  String url = "http://www.1080x1920wallpapers.com/1080x1920-backgrounds/1080x1920-wallpapers-2/1080x1920-HD-wallpapers-samsung-htc-android-smartphone-642srwg4-1080P.jpg";
+    String url = "http://"+ ipadress + ":8080/MatchIDEnterpriseApp-war/rest/getImage/imageid/3";
     private ImageView ivFoto;
 
     private View view;
-    private ArrayAdapter<String> adapter;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -77,11 +54,9 @@ public class PictureViewFragment extends Fragment {
     }
 
     // TODO: Rename and change types and number of parameters
-    public static PictureViewFragment newInstance(String param1, String param2) {
+    public static PictureViewFragment newInstance() {
         PictureViewFragment fragment = new PictureViewFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -100,26 +75,13 @@ public class PictureViewFragment extends Fragment {
         view = inflater.inflate(R.layout.picture, container, false);
         ivFoto = (ImageView) view.findViewById(R.id.fotoweertegeven);
         loadImageFromURL(url);
-
+        //view image
         return view;
     }
 
     private void loadImageFromURL(String url) {
-        Picasso.with(getActivity()).load(url).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher)
-                .into(ivFoto , new com.squareup.picasso.Callback() {
-                    @Override
-                    public void onSuccess() {
-
-                    }
-
-                    @Override
-                    public void onError() {
-
-                    }
-                });
-
-
-
+        //ImageView ivFoto = (ImageView) findViewById(R.id.chart_image);
+        new XMLTask().execute(url);
     }
 
 
@@ -146,51 +108,28 @@ public class PictureViewFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 
     public class XMLTask extends AsyncTask<String, String, String> {
-
-        private TextView tv;
-        private View view;
-        private ListView lv;
-        private List<String> strArr;
-        private ArrayAdapter<String> adapter;
-
         @Override
         protected String doInBackground(String... urls) {
             HttpURLConnection connection = null;
             BufferedReader reader = null;
-
             try {
                 java.net.URL url = new URL(urls[0]);
                 connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
-
                 InputStream stream = connection.getInputStream();
-
                 reader = new BufferedReader(new InputStreamReader(stream));
                 StringBuffer buffer = new StringBuffer();
-
                 String line = "";
                 while ((line = reader.readLine()) != null) {
                     buffer.append(line);
                 }
-
                 return buffer.toString();
-
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -207,20 +146,19 @@ public class PictureViewFragment extends Fragment {
                     e.printStackTrace();
                 }
             }
-
             return null;
         }
 
         @Override
         protected void onPostExecute(String line) {
             super.onPostExecute(line);
-            //deze onPost wordt uitgevoerd als er iets terug gegeven is
-            Log.d("tag", line);
-            //line is een string
-
-
+            byte[] imageByteArray = Base64.decode(line , Base64.DEFAULT);
+            try {
+                Bitmap bmp = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.length);
+                ivFoto.setImageBitmap(bmp);
+            }catch (Exception e){
+                Log.d("tag" , e.toString());
+            }
         }
     }
-
-
 }
