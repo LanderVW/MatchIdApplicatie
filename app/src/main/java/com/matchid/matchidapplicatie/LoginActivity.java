a5f0e76c2345fa0cc1dac1e59bf37e2313df9929
VanWassenhoveLander <lander.vanwassenhove@student.kuleuven.be>
2017-04-25 09:14:21 +0200

    ik kan inloggen

--- app/src/main/java/com/matchid/matchidapplicatie/LoginActivity.java
+++ app/src/main/java/com/matchid/matchidapplicatie/LoginActivity.java
@@ -26,9 +26,6 @@ import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import com.matchid.matchidapplicatie.entities.SessionManager;
 
 import java.net.UnknownHostException;
@@ -39,18 +36,18 @@ import static android.graphics.Typeface.BOLD;
 
 public class LoginActivity extends Activity implements QuitDialog.Communicator{
     Button btn_signin, btn_cancel;
-    Button btn_skip_login;
     EditText etUsername, etPassword;
     TextView matchid_logo, error_message;
     private ProgressBar spinner;
+    private static final String TAG = "LoginActivity";
     public static final String KEY_PRIVATE = "USERNAME";
-    static final String ipadress = "192.168.0.234";
     String name;
-    String userId;
+    static String userId;
     Boolean logout;
 
     static int id =0;
 
+    static final String ipadress = "10.110.129.10";
     SessionManager session;
 
 
@@ -65,15 +62,6 @@ public class LoginActivity extends Activity implements QuitDialog.Communicator{
         etPassword = (EditText)findViewById(R.id.password);
         btn_signin = (Button)findViewById(R.id.btn_signin);
         btn_cancel = (Button)findViewById(R.id.btn_cancel);
-        btn_skip_login = (Button) findViewById(R.id.skip_login);
-
-        btn_skip_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent skipLogin = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(skipLogin);
-            }
-        });
 
         error_message=(TextView)findViewById(R.id.error_message);
         error_message.setVisibility(TextView.INVISIBLE);
@@ -103,17 +91,20 @@ public class LoginActivity extends Activity implements QuitDialog.Communicator{
         if(check){
             Intent goHome = new Intent(getApplicationContext(), MainActivity.class);
             HashMap<String, String> user = session.getUserDetails();
+            userId = user.get(SessionManager.userId);
             Toast.makeText(LoginActivity.this, "Welcome " + user.get(SessionManager.KEY_NAME) , Toast.LENGTH_SHORT).show();
+
             startActivity(goHome);
         }
     }
 
-    private static String getValue(String tag, Element element) {
-        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
-        Node node = nodeList.item(0);
-        return node.getNodeValue();
+    public String getUserId() {
+        return userId;
     }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
     @Override
     public void onBackPressed() {
         showDialog(this.findViewById(R.id.flContent));
@@ -129,7 +120,6 @@ public class LoginActivity extends Activity implements QuitDialog.Communicator{
 
     public String getUsername(){
         SharedPreferences sp = getSharedPreferences("userinfo", Context.MODE_PRIVATE);
-
         return sp.getString("name", "fout");
     }
 
@@ -148,7 +138,6 @@ public class LoginActivity extends Activity implements QuitDialog.Communicator{
         String url ="http://"+ipadress+":8080/MatchIDEnterpriseApp-war/LoginServlet?username="+ etUsername.getText()+
                 "&password="+ etPassword.getText()+"&android=true";
 
-
         // Formulate the request and handle the response.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -156,12 +145,14 @@ public class LoginActivity extends Activity implements QuitDialog.Communicator{
                     @Override
                     public void onResponse(String response) {
                         // Do something with the response
-                        if(response.equalsIgnoreCase("ok")){
-
-                            Log.d("LoginActivity", "login succesvol");
-                        //if(!response.equalsIgnoreCase("nowp")){
-                          //id = Integer.parseInt(response);
-                            Log.d("LoginActivity" , response);
+                        if(!response.equalsIgnoreCase("Error")){
+                            userId = response;
+                            Log.d(TAG , "het userid is: " + userId);
+//                            Log.d(TAG , response);
+//                            Log.d(TAG, "login succesvol");
+                            //if(!response.equalsIgnoreCase("nowp")){
+                            //id = Integer.parseInt(response);
+                            Log.d(TAG , response);
 
                             Intent goHome = new Intent(getApplicationContext(), MainActivity.class);
                             spinner.setVisibility(View.GONE);
@@ -169,26 +160,29 @@ public class LoginActivity extends Activity implements QuitDialog.Communicator{
                             //goHome.putExtra("username", etUsername.getText().toString());
                             //goHome.putExtra("ipadres", ipadress);
                             //safeInfo();
-                            session.createLoginSession(etUsername.getText().toString() , 8);
+                            session.createLoginSession(etUsername.getText().toString() , userId);
                             startActivity(goHome);
 
                             spinner.setVisibility(View.GONE);
                         }else{
-                            Log.d("LoginActivity",response);
+                            Log.d(TAG, "tesst " +response);
+                            spinner.setVisibility(View.GONE);
                         }
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG,"error bij login");
+                        spinner.setVisibility(View.GONE);
                         // Handle error
                         if(error.toString().contains("TimeoutError")||error.toString().contains("NoConnectionError")){
-                            error_message.setText("Er is iets foutgelopen.\nCheck je connectie en probeer opnieuw.");
-
+                            //error_message.setText("Er is iets foutgelopen.\nCheck je connectie en probeer opnieuw.");
+                            error_message.setText(error.toString());
                         }else error_message.setText(error.toString());
 
                         error_message.setVisibility(TextView.VISIBLE);
-                        spinner.setVisibility(View.GONE);
+
                     }
                 });
 
@@ -205,4 +199,3 @@ public class LoginActivity extends Activity implements QuitDialog.Communicator{
 
 
 }
-
--- app/src/main/java/com/matchid/matchidapplicatie/entities/SessionManager.java
+++ app/src/main/java/com/matchid/matchidapplicatie/entities/SessionManager.java
@@ -1,15 +1,11 @@
 package com.matchid.matchidapplicatie.entities;
 
-import java.util.HashMap;
-
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.util.Log;
 
-import com.matchid.matchidapplicatie.LoginActivity;
-import com.matchid.matchidapplicatie.MainActivity;
+import java.util.HashMap;
 
 /**
  * Created by vulst on 23/04/2017.
@@ -17,103 +13,98 @@ import com.matchid.matchidapplicatie.MainActivity;
 
 public class SessionManager {
 
-        // Shared Preferences
-        SharedPreferences pref;
+    // Shared Preferences
+    SharedPreferences pref;
 
-        // Editor for Shared preferences
-        Editor editor;
+    // Editor for Shared preferences
+    Editor editor;
 
-        // Context
-        Context _context;
+    // Context
+    Context _context;
 
-        // Shared pref mode
-        int PRIVATE_MODE = 0;
+    // Shared pref mode
+    int PRIVATE_MODE = 0;
 
-        // Sharedpref file name
-        private static final String PREF_NAME = "AndroidHivePref";
+    // Sharedpref file name
+    private static final String PREF_NAME = "AndroidHivePref";
 
-        // All Shared Preferences Keys
-        private static final String IS_LOGIN = "IsLoggedIn";
+    // All Shared Preferences Keys
+    private static final String IS_LOGIN = "IsLoggedIn";
 
-        // User name (make variable public to access from outside)
-        public static final String KEY_NAME = "name";
+    // User name (make variable public to access from outside)
+    public static final String KEY_NAME = "name";
 
-        // Email address (make variable public to access from outside)
-        public static final String userId = "id";
+    // Email address (make variable public to access from outside)
+    public static final String userId = "id";
 
-        // Constructor
-        public SessionManager(Context context){
-            this._context = context;
-            pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
-            editor = pref.edit();
-            Log.d("tag" , "in sessionmanager context maker");
+    // Constructor
+    public SessionManager(Context context){
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+        Log.d("tag" , "in sessionmanager context maker");
+    }
+
+    /**
+     * Create login session
+     * */
+    public void createLoginSession(String name, String  id){
+        // Storing login value as TRUE
+
+        editor.putBoolean(IS_LOGIN, true);
+        // Storing name in pref
+        editor.putString(KEY_NAME, name);
+        // Storing email in pref
+        editor.putString(userId , id);
+        // commit changes
+        editor.commit();
+    }
+
+    /**
+     * Check login method wil check user login status
+     * If false it will redirect user to login page
+     * Else won't do anything
+     * */
+    public boolean checkLogin(){
+        //check login status
+        boolean back =false;
+        if(this.isLoggedIn()) {
+            back =true;
         }
-
-        /**
-         * Create login session
-         * */
-        public void createLoginSession(String name, Integer  id){
-            // Storing login value as TRUE
-
-            editor.putBoolean(IS_LOGIN, true);
-            // Storing name in pref
-            editor.putString(KEY_NAME, name);
-            // Storing email in pref
-            editor.putInt(userId , id);
-            // commit changes
-            editor.commit();
+        else{
         }
-
-        /**
-         * Check login method wil check user login status
-         * If false it will redirect user to login page
-         * Else won't do anything
-         * */
-        public boolean checkLogin(){
-            //check login status
-            boolean back =false;
-            if(this.isLoggedIn()) {
-                Log.d("tag" ,"dosomethaihgid");
-                back =true;
-            }
-            else{
-                Log.d("tag" , "faoutjfdjfjakt");
-            }
-            return back;
-        }
+        return back;
+    }
 
     // Get Login State
     public boolean isLoggedIn(){
         return pref.getBoolean(IS_LOGIN, false);
     }
-       /**
-         * Get stored session data
-         * */
-        public HashMap<String, String> getUserDetails(){
-            HashMap<String, String> user = new HashMap<String, String>();
-            // user name
-            user.put(KEY_NAME, pref.getString(KEY_NAME, null));
-            user.put(userId , userId.toString() );
+    /**
+     * Get stored session data
+     * */
+    public HashMap<String, String> getUserDetails(){
+        HashMap<String, String> user = new HashMap<String, String>();
+        // user name
+        user.put(KEY_NAME, pref.getString(KEY_NAME, null));
+        user.put(userId , pref.getString(userId  ,null));
 
-            // user id
-           // user.put(userId, pref.getInt(userId , ));
+        // return user
+        return user;
+    }
 
-            // return user
-            return user;
-        }
+    /**
+     * Clear session details
+     * */
+    public void logoutUser(){
+        // Clearing all data from Shared Preferences
+        editor.clear();
+        editor.commit();
+    }
 
-        /**
-         * Clear session details
-         * */
-        public void logoutUser(){
-            // Clearing all data from Shared Preferences
-            editor.clear();
-            editor.commit();
-        }
-
-        /**
-         * Quick check for login
-         * **/
+    /**
+     * Quick check for login
+     * **/
 
 
 }
